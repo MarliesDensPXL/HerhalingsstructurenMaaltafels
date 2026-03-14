522bef243537776b160b251a1104e677db0b0b5b
Marlies Dens <marlies.dens@pxl.be>
2026-03-14 17:09:07 +0100

    Add project files.

---
+++ Maaltafels/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Maaltafels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("De maaltafels");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            int choice;
+            bool isValid;
+
+            do
+            {
+
+                Console.Write("Welke tafel wil je oefenen? ");
+                isValid = int.TryParse(Console.ReadLine(), out choice);
+                Console.WriteLine();
+
+            } while (!isValid);
+                       
+
+            for (int i = 0; i < 11; i++)
+            {
+
+                int result;
+                do
+                {
+                    Console.Write($"{i} x {choice} = ");
+                    int.TryParse(Console.ReadLine(), out result);
+                } while (result != (i * choice));
+
+                
+            }
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Proficiat! Je kent de tafel van {choice}!");
+            Console.ResetColor();
+
+            
+
+        }
+    }
+}
