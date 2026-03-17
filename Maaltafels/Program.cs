4cb05fe47712fbcfb023d944e44f7f71f4f435fd
Marlies Dens <marlies.dens@pxl.be>
2026-03-17 16:50:48 +0100

    methods toegevoegd

--- Maaltafels/Program.cs
+++ Maaltafels/Program.cs
@@ -4,104 +4,58 @@
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("De maaltafels");
-            Console.ResetColor();
-            Console.WriteLine();
-
-            int choice;
-            bool isValid;
-            string repeat;
             Random rng = new Random();
+
+            ShowColoredText("De maaltafels", ConsoleColor.DarkYellow, true);
+
+            //Console.ForegroundColor = ConsoleColor.DarkYellow;
+            //Console.WriteLine("De maaltafels");
+            //Console.ResetColor();
+            //Console.WriteLine();
+
+            string repeat;
 
             do
             {
-                do
-                {
-                    Console.ForegroundColor = ConsoleColor.Magenta;
-                    Console.Write("Welke tafel wil je oefenen? ");
-                    isValid = (int.TryParse(Console.ReadLine(), out choice) && choice > 0);
-                    Console.ResetColor();
-                    Console.WriteLine();
 
-                } while (!isValid);
+                PracticeTableRow();
 
-
-                for (int i = 0; i < 11; i++)
-                {
-
-                    int result;
-                    do
-                    {
-                        Console.Write($"{i} x {choice} = ");
-                        int.TryParse(Console.ReadLine(), out result);
-                    } while (result != (i * choice));
-
-
-                }
-
-                Console.WriteLine();
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"Proficiat! Je kent de tafel van {choice}!");
-                Console.ResetColor();
-
-                Console.Write("Wil je nog een tafel oefenen? ja/nee: ");
+                ShowColoredText("Wil je nog meer oefenen? ja/ nee: ", ConsoleColor.White, false);
+                
                 repeat = Console.ReadLine();
                 Console.WriteLine();
-
             } while (repeat.Equals("ja"));
 
-
-
-
             Console.WriteLine();
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine("En nu alles door elkaar!");
-            Console.ResetColor();
-            Console.WriteLine();
-
+            ShowColoredText("Nu alles door elkaar!", ConsoleColor.Magenta, true);
             
+            //Console.ForegroundColor = ConsoleColor.Magenta;
+            //Console.WriteLine("Nu alles door elkaar!");
+            //Console.ResetColor();
+            //Console.WriteLine();
 
-            
+                       
 
             
 
             do
             {
                 Console.Write("Wat kies je? x of : ");
-                string bewerking = Console.ReadLine();
+                string operation = Console.ReadLine();
 
-                switch (bewerking)
+                switch (operation)
                 {
 
                     case "x":
-                        for (int i = 0; i < 10; i++)
-                        {
-
-                            int number1 = rng.Next(1, 11);
-                            int number2 = rng.Next(1, 11);
-                            int result;
-                            do
-                            {
-                                Console.Write($"{number1} x {number2} = ");
-                                int.TryParse(Console.ReadLine(), out result);
-                            } while (result != (number1 * number2));
-                        }
+                        PracticeMultiply();
                         break;
 
                     case ":":
-                        for (int i = 0; i < 10; i++)
-                        {
-
-                            int number1 = rng.Next(1, 11);
-                            int number2 = rng.Next(1, 11);
-                            int result;
-                            do
-                            {
-                                Console.Write($"{number1 * number2} : {number2} = ");
-                                int.TryParse(Console.ReadLine(), out result);
-                            } while (result != number1);
-                        }
+                        PracticeDivision();
+                        break;
+                    default:
+                        ShowColoredText("Ongeldige invoer!", ConsoleColor.Red, true);
+                        
                         break;
 
                 }
@@ -109,16 +63,121 @@
                                
 
                 Console.WriteLine();
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Proficiat! Goed geoefend!");
-                Console.WriteLine();
-                Console.ResetColor();
+                ShowColoredText("Proficiat! Goed geoefend!", ConsoleColor.Green, true);
 
-                Console.Write("Wil je nog meer oefenen? ja/nee: ");
+                //Console.ForegroundColor = ConsoleColor.Green;
+                //Console.WriteLine("Proficiat! Goed geoefend!");
+                //Console.WriteLine();
+                //Console.ResetColor();
+
+                ShowColoredText("Wil je nog meer oefenen? ja/ nee: ", ConsoleColor.White, false);
+
                 repeat = Console.ReadLine();
                 Console.WriteLine();
 
+
             } while (repeat.Equals("ja"));
         }
-    }
+
+        private static void ShowColoredText(string text, ConsoleColor color, bool appendLine)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            if (appendLine)
+            {
+                Console.WriteLine();
+            }
+            Console.ResetColor();
+
+        }
+
+        private static void PracticeTableRow()
+        {
+            int choice;
+            bool isValid;
+            
+            
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.Write("Welke tafel wil je oefenen? ");
+                isValid = (int.TryParse(Console.ReadLine(), out choice) && choice > 0 && choice <= 10);
+                Console.ResetColor();
+                Console.WriteLine();
+
+            } while (!isValid);
+
+
+            for (int i = 0; i < 11; i++)
+            {
+                int result;
+
+                do
+                {
+
+                    Console.Write($"{i} x {choice} = ");
+                    int.TryParse(Console.ReadLine(), out result);
+                    if (result != (i * choice))
+                    {
+                        ShowColoredText("Oeps. Probeer nog een keer!", ConsoleColor.Red, true);
+                        
+                    }
+                } while (result != (i * choice));
+
+
+            }
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Proficiat! Je kent de tafel van {choice}!");
+            Console.ResetColor();
+                             
+
+
+        }
+
+        private static void PracticeMultiply()
+        {
+            Random rng = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+
+                int number1 = rng.Next(1, 11);
+                int number2 = rng.Next(1, 11);
+                int result;
+                do
+                {
+                    Console.Write($"{number1} x {number2} = ");
+                    int.TryParse(Console.ReadLine(), out result);
+                    if (result != (number1 * number2))
+                        {
+                        ShowColoredText("Bijna... Probeer nog een keer!", ConsoleColor.Red, true);
+                        }
+                } while (result != (number1 * number2));
+            }
+        }
+
+        private static void PracticeDivision()
+        {
+
+            Random rng = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+
+                int number1 = rng.Next(1, 11);
+                int number2 = rng.Next(1, 11);
+                int result;
+                do
+                {
+                    Console.Write($"{number1 * number2} : {number2} = ");
+                    int.TryParse(Console.ReadLine(), out result);
+                    if (result != number1)
+                    {
+                        ShowColoredText("Oeps. Nog eens proberen!", ConsoleColor.Red, true);
+                    }
+                } while (result != number1);
+            }
+        }
+}
 }
